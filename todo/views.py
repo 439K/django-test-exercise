d60357d6cf91d69738e0edd2a61f4f0d377ccdee
K <136606225+s1f10230145@users.noreply.github.com>
2024-07-11 14:33:55 +0900

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -34,7 +34,7 @@ def detail(request, task_id):
     return render(request, "todo/detail.html", context)
 
 
-def clone(request, task_id):
+def close(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
