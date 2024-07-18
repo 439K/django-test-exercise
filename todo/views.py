a12ed505f6f7cfd292ba1f8028b59b9f17b13e36
1F10230145 <s1f102301459@iniad.org>
2024-07-18 13:29:01 +0900

    バグの修正

--- todo/views.py
+++ todo/views.py
@@ -57,10 +57,11 @@ def close(request, task_id):
         raise Http404("Task does not exist")
     task.completed = True
     task.save()
+    return redirect(index)
 
 def delete(request, task_id):
     try:
-        task = Task.object.get(pk=task_id)
+        task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task dose not exist")
     task.delete()
